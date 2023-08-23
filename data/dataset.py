48bc6abea2828f4ec7ee16b5ffd338c133bb8460
WojciechMat <wmatsad@gmail.com>
2023-08-23 16:06:52 +0200

    begin working on dataset

--- data/dataset.py
+++ data/dataset.py
@@ -1,11 +1,61 @@
-import pandas as pd
+import itertools
+
+import fortepyan as ff
+from tqdm import tqdm
 from datasets import load_dataset
+from prepare_dataset import process_record
+
+from data.quantizer import MidiQuantizer
 
 
-class MidiTranslationDataset:
+class TokenizedMidiTranslationDataset:
     def __init__(
-            self,
-            split: str = "train",
+        self,
+        split: str = "train",
+        n_dstart_bins: int = 3,
+        n_duration_bins: int = 10,
+        n_velocity_bins: int = 10,
+        sequence_len: int = 128,
     ):
-        self.dataset = load_dataset("roszcz/maestro-v1", split=split)
+        self.sequence_len = sequence_len
+        self.quantizer = MidiQuantizer(
+            n_dstart_bins=n_dstart_bins,
+            n_duration_bins=n_duration_bins,
+            n_velocity_bins=n_velocity_bins,
+        )
+        self.dataset = load_dataset(path="roszcz/maestro-v1", split=split)
+        self.processed_records = []
         self.samples = []
+        self.vocab_src, self.vocab_tgt = self._build_vocab()
+        self._build()
+
+    def _build(self):
+        for record in tqdm(self.dataset, total=self.dataset.num_rows):
+            piece = ff.MidiPiece.from_huggingface(record)
+            processed_record = process_record(piece, self.sequence_len, self.quantizer)
+
+            self.processed_records += processed_record
+
+    def _build_vocab(self) -> tuple[list[str], list[str]]:
+        vocab_src, vocab_tgt = [], []
+
+        # every combination of pitch + dstart
+        product = itertools.product(range(21, 109), range(self.quantizer.n_dstart_bins))
+        for pitch, dstart in product:
+            key = f"{pitch}-{dstart}"
+            vocab_src.append(key)
+
+        # every combination of duration + velocity
+        product = itertools.product(range(self.quantizer.n_duration_bins), range(self.quantizer.n_velocity_bins))
+        for duration, velocity in product:
+            key = f"{duration}-{velocity}"
+            vocab_tgt.append(key)
+        return vocab_src, vocab_tgt
+
+    def __getitem__(self, idx: int):
+        return self.processed_records[idx]
+
+
+if __name__ == "__main__":
+    dataset = TokenizedMidiTranslationDataset()
+    print(dataset[0])
---
+++ data/quantizer.py
@@ -0,0 +1,116 @@
+"""
+MidiQuantizer by JasiekKaczmarczyk
+from https://github.com/Nospoko/midi-clip/blob/MIDI-44/midi-clip/data/quantizer.py
+"""
+
+import yaml
+import numpy as np
+import pandas as pd
+from fortepyan import MidiPiece
+from hydra.utils import to_absolute_path
+
+
+class MidiQuantizer:
+    def __init__(
+        self,
+        n_dstart_bins: int = 3,
+        n_duration_bins: int = 3,
+        n_velocity_bins: int = 3,
+    ):
+        self.n_dstart_bins = n_dstart_bins
+        self.n_duration_bins = n_duration_bins
+        self.n_velocity_bins = n_velocity_bins
+
+        self._build()
+
+    def __rich_repr__(self):
+        yield "MidiQuantizer"
+        yield "n_dstart_bins", self.n_dstart_bins
+        yield "n_duration_bins", self.n_duration_bins
+        yield "n_velocity_bins", self.n_velocity_bins
+
+    def _build(self):
+        self._load_bin_edges()
+        self._build_dstart_decoder()
+        self._build_duration_decoder()
+        self._build_velocity_decoder()
+
+    def _load_bin_edges(self):
+        # Hydra changes paths, this finds it back
+        artifacts_path = to_absolute_path("../artifacts/bin_edges.yaml")
+        with open(artifacts_path, "r") as f:
+            bin_edges = yaml.safe_load(f)
+
+        self.dstart_bin_edges = bin_edges["dstart"][self.n_dstart_bins]
+        self.duration_bin_edges = bin_edges["duration"][self.n_duration_bins]
+        self.velocity_bin_edges = bin_edges["velocity"][self.n_velocity_bins]
+
+    def quantize_piece(self, piece: MidiPiece) -> MidiPiece:
+        # Try not to overwrite anything
+        df = piece.df.copy()
+        source = dict(piece.source) | {"quantized": True}
+
+        # Make the quantization
+        df = self.quantize_frame(df)
+
+        out = MidiPiece(df=df, source=source)
+        return out
+
+    def quantize_frame(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["next_start"] = df.start.shift(-1)
+        df["dstart"] = df.next_start - df.start
+
+        df["dstart_bin"] = np.digitize(df.dstart.fillna(0), self.dstart_bin_edges) - 1
+        df["duration_bin"] = np.digitize(df.duration, self.duration_bin_edges) - 1
+        df["velocity_bin"] = np.digitize(df.velocity, self.velocity_bin_edges) - 1
+
+        df = self.apply_quantization(df)
+        return df
+
+    def apply_quantization(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["quant_dstart"] = df.dstart_bin.map(lambda it: self.bin_to_dstart[it])
+        df["quant_duration"] = df.duration_bin.map(lambda it: self.bin_to_duration[it])
+        df["start"] = df.quant_dstart.cumsum().shift(1).fillna(0)
+        df["end"] = df.start + df.quant_duration
+        df["duration"] = df.quant_duration
+        df["velocity"] = df.velocity_bin.map(lambda it: self.bin_to_velocity[it])
+        return df
+
+    def _build_duration_decoder(self):
+        self.bin_to_duration = []
+        for it in range(1, len(self.duration_bin_edges)):
+            duration = (self.duration_bin_edges[it - 1] + self.duration_bin_edges[it]) / 2
+            self.bin_to_duration.append(duration)
+
+        last_duration = 2 * self.duration_bin_edges[-1]
+        self.bin_to_duration.append(last_duration)
+
+    def _build_dstart_decoder(self):
+        self.bin_to_dstart = []
+        for it in range(1, len(self.dstart_bin_edges)):
+            dstart = (self.dstart_bin_edges[it - 1] + self.dstart_bin_edges[it]) / 2
+            self.bin_to_dstart.append(dstart)
+
+        last_dstart = 2 * self.dstart_bin_edges[-1]
+        self.bin_to_dstart.append(last_dstart)
+
+    def _build_velocity_decoder(self):
+        # For velocity the first bin is not going to be
+        # evenly populated, skewing towards to higher values
+        # (who plays with velocity 0?)
+        self.bin_to_velocity = [int(0.8 * self.velocity_bin_edges[1])]
+
+        for it in range(2, len(self.velocity_bin_edges)):
+            dstart = (self.velocity_bin_edges[it - 1] + self.velocity_bin_edges[it]) / 2
+            self.bin_to_velocity.append(int(dstart))
+
+    def make_vocab(self) -> list[str]:
+        vocab = []
+        for it, pitch in enumerate(range(21, 109)):
+            for jt in range(self.n_duration_bins):
+                for kt in range(self.n_dstart_bins):
+                    for vt in range(self.n_velocity_bins):
+                        key = f"{kt}_{jt}_{vt}_{pitch}"
+                        vocab.append(key)
+
+        return vocab
