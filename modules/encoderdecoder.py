8bfbe6de08a2b723ab78e63cb52d7fabef63e8f5
WojciechMat <wmatsad@gmail.com>
2023-08-29 09:57:24 +0200

    delete unnecessary and add hydra-core to requirements

--- modules/encoderdecoder.py
+++ modules/encoderdecoder.py
@@ -1,10 +1,7 @@
 import torch
 import pandas as pd
-import altair as alt
 import torch.nn as nn
 from torch.nn.functional import log_softmax
-
-alt.renderers.enable("altair_viewer")
 
 
 class EncoderDecoder(nn.Module):
@@ -62,35 +59,3 @@ def subsequent_mask(size: int):
     subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1).type(torch.uint8)
     return subsequent_mask == 0
 
-
-def example_mask():
-    LS_data = pd.concat(
-        [
-            pd.DataFrame(
-                {
-                    "Subsequent Mask": subsequent_mask(20)[0][x, y].flatten(),
-                    "Window": y,
-                    "Masking": x,
-                }
-            )
-            for y in range(20)
-            for x in range(20)
-        ]
-    )
-
-    return (
-        alt.Chart(LS_data)
-        .mark_rect()
-        .properties(height=250, width=250)
-        .encode(
-            alt.X("Window:O"),
-            alt.Y("Masking:O"),
-            alt.Color("Subsequent Mask:Q", scale=alt.Scale(scheme="viridis")),
-        )
-        .interactive()
-    )
-
-
-if __name__ == "__main__":
-    chart = example_mask()
-    chart.show()
