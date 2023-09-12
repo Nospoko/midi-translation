e18d68017356702a6216c966d323966502a6552b
WojciechMat <wmatsad@gmail.com>
2023-09-12 22:06:55 +0200

    rename script for renaming full file-paths to ones compatible with doc.py, where I do no need info on okenization method or model used, as there is only one global setting(for now)

---
+++ documentation/files/samples/rename.py
@@ -0,0 +1,22 @@
+import copy
+import os
+idx = 0
+it = 0
+
+for f in os.listdir():
+    file = copy.copy(f)
+    if file.split('.')[1] == "py" or file.split('-')[1].split('.')[0] in ["qv", "real", "predicted", "quantized"]:
+        continue
+    if file.find("real") > 0:
+        idx = file.split('-')[-6]
+        file = f"{idx}-real." + file.split('.')[1]
+    elif file.find("qv") > 0:
+        idx = file.split('-')[-6]
+        file = f"{idx}-qv." + file.split('.')[1]
+    elif file.find("quantized") > 0:
+        idx = file.split('-')[-6]
+        file = f"{idx}-quantized." + file.split('.')[1]
+    else:
+        idx = file.split('-')[-1].split('.')[0]
+        file = f"{idx}-predicted." + file.split('.')[1]
+    os.rename(f, file)
