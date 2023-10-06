3d1d1c7b76ecda8c999176b04e86febbbd6ac11e
WojciechMat <wmatsad@gmail.com>
2023-10-06 12:46:01 +0200

    dstart prediction, remove encode_add_cls function

--- app/main.py
+++ app/main.py
@@ -1,39 +1,63 @@
+import glob
+
 import torch
 import gradio as gr
 import fortepyan as ff
 from omegaconf import OmegaConf
 from fortepyan.audio import render as render_audio
 
-from app.tools import load_model, predict_velocity
+from app.tools import load_model, predict_velocity, predict_dstart
 
 
-def run_app(target: str, midi_file):
-    checkpoint = torch.load("checkpoints/velocity/midi-transformer-2023-10-03-08-34.pt", map_location="cpu")
+def run_dstart_app(midi_file, model_path: str):
+    checkpoint = torch.load(model_path, map_location="cpu")
     model = load_model(checkpoint)
     train_cfg = OmegaConf.create(checkpoint["cfg"])
 
     piece = ff.MidiPiece.from_file(midi_file.name)
-    if target == "velocity":
-        pred_piece = predict_velocity(model, train_cfg, piece)
-    else:
-        # TODO: predict_dstart
-        pred_piece = predict_velocity(model, train_cfg, piece)
+    predicted_piece = predict_dstart(model, train_cfg, piece)
 
-    audio_path = render_audio.midi_to_mp3(pred_piece.to_midi(), "tmp/predicted-audio.mp3")
+    audio_path = render_audio.midi_to_mp3(predicted_piece.to_midi(), "tmp/predicted-audio.mp3")
+    audio = gr.make_waveform(audio_path)
+
+    return audio
+
+
+def run_velocity_app(midi_file, model_path: str):
+    checkpoint = torch.load(model_path, map_location="cpu")
+    model = load_model(checkpoint)
+    train_cfg = OmegaConf.create(checkpoint["cfg"])
+
+    piece = ff.MidiPiece.from_file(midi_file.name)
+    predicted_piece = predict_velocity(model, train_cfg, piece)
+
+    audio_path = render_audio.midi_to_mp3(predicted_piece.to_midi(), "tmp/predicted-audio.mp3")
     audio = gr.make_waveform(audio_path)
 
     return audio
 
 
 def main():
-    demo = gr.Interface(
-        run_app,
-        inputs=[
-            gr.components.Radio(["velocity"]),
-            gr.File(),
-        ],
-        outputs=[gr.Video()],
-    )
+    with gr.Blocks() as demo:
+        file = gr.File(file_count="single", label="midi file"),
+        with gr.Tab("Predict Velocity"):
+            with gr.Row():
+                with gr.Column():
+                    velocity_model_path = gr.Dropdown(glob.glob("checkpoints/velocity/*.pt"), label="model")
+                    velocity_button = gr.Button("Predict")
+                with gr.Column():
+                    velocity_out = gr.Video()
+
+        with gr.Tab("Predict Dstart"):
+            with gr.Row():
+                with gr.Column():
+                    dstart_model_path = gr.Dropdown(glob.glob("checkpoints/dstart/*.pt"), label="model")
+                    dstart_button = gr.Button("Predict")
+                with gr.Column():
+                    dstart_out = gr.Video()
+
+        velocity_button.click(run_velocity_app, inputs=[file[0], velocity_model_path], outputs=velocity_out)
+        dstart_button.click(run_dstart_app, inputs=[file[0], dstart_model_path], outputs=dstart_out)
 
     demo.launch(server_name="0.0.0.0")
 
--- app/tools.py
+++ app/tools.py
@@ -8,13 +8,7 @@ from model import make_model
 from data.quantizer import MidiQuantizer
 from utils import vocab_sizes, greedy_decode
 from data.dataset import quantized_piece_to_records
-from data.tokenizer import VelocityEncoder, QuantizedMidiEncoder
-
-
-def encode_add_cls(sequence: dict, encoder: QuantizedMidiEncoder) -> torch.Tensor:
-    token_ids = encoder.encode(sequence)
-    token_ids = [encoder.token_to_id["<CLS>"]] + token_ids
-    return torch.tensor(token_ids, dtype=torch.int64)
+from data.tokenizer import VelocityEncoder, DstartEncoder, QuantizedMidiEncoder
 
 
 def process_piece(train_cfg: DictConfig, piece: ff.MidiPiece, quantizer: MidiQuantizer) -> list[torch.Tensor]:
@@ -30,7 +24,13 @@ def process_piece(train_cfg: DictConfig, piece: ff.MidiPiece, quantizer: MidiQua
         sequence_len=train_cfg.dataset.sequence_len,
         sequence_step=train_cfg.dataset.sequence_len,
     )
-    sequence_tokens = [encode_add_cls(sequence, src_encoder) for sequence in sequences]
+    sequence_tokens = [
+        torch.tensor(
+            [[src_encoder.token_to_id["<CLS>"]] + src_encoder.encode(sequence)],
+            dtype=torch.int64
+        )
+        for sequence in sequences
+    ]
 
     return sequence_tokens
 
@@ -49,6 +49,42 @@ def load_model(checkpoint: dict) -> nn.Module:
     model.to("cpu")
     model.load_state_dict(checkpoint["model_state_dict"])
     return model
+
+
+def predict_dstart(model: nn.Module, train_cfg: DictConfig, piece: ff.MidiPiece) -> ff.MidiPiece:
+    tgt_encoder = DstartEncoder(n_bins=train_cfg.dstart_bins)
+    quantizer = MidiQuantizer(
+        n_dstart_bins=train_cfg.dataset.quantization.dstart,
+        n_duration_bins=train_cfg.dataset.quantization.duration,
+        n_velocity_bins=train_cfg.dataset.quantization.velocity,
+    )
+    sequences = process_piece(train_cfg=train_cfg, piece=piece, quantizer=quantizer)
+
+    predicted_tokens = []
+    for record in tqdm(sequences):
+        src_token_ids = record
+
+        predicted_token_ids = greedy_decode(
+            model=model,
+            src=src_token_ids,
+            max_len=train_cfg.dataset.sequence_len,
+        )
+
+        out_tokens = [tgt_encoder.vocab[x] for x in predicted_token_ids]
+        predicted_tokens += out_tokens
+
+    predictions = tgt_encoder.untokenize(predicted_tokens)
+
+    predicted_piece_df = piece.df.copy()
+    predicted_piece_df = predicted_piece_df.head(len(predictions))
+
+    predicted_piece_df["start"] = tgt_encoder.unquantized_start(predictions)
+    predicted_piece_df["end"] = predicted_piece_df["start"] + predicted_piece_df["duration"]
+    predicted_piece = ff.MidiPiece(predicted_piece_df)
+
+    predicted_piece.source = piece.source.copy()
+
+    return predicted_piece
 
 
 def predict_velocity(model: nn.Module, train_cfg: DictConfig, piece: ff.MidiPiece):
